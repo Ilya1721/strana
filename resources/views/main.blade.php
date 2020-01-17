5159ad0a833b32c8598d4041310c30ea4796fc74
unknown <ilyavusnya@gmail.com>
2020-01-17 19:41:43 +0200

    Header

---
+++ resources/views/main.blade.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <link href="css/main.css" rel="stylesheet">
+
+  </head>
+  <body>
+    <div class="page">
+      <div class="outer-div"></div>
+      <div class="main-page">
+        <header class="header">
+          <div class="upper-header">
+            <div class="header-img-div">
+              <a href="/">
+                <img src="img/logo.png" alt="logo">
+              </a>
+            </div>
+          </div>
+          <nav class="navbar">
+            <ul>
+              <li id="nav-first-item">
+                <a href="#">Новости</a>
+              </li>
+              <li>
+                <a href="#">Статьи</a>
+              </li>
+              <li>
+                <a href="#">Интервью</a>
+              </li>
+              <li>
+                <a href="#">Лента Соцсетей</a>
+              </li>
+              <li>
+                <a href="#">Видео</a>
+              </li>
+              <li>
+                <a href="#">Атака на Страну</a>
+              </li>
+              <li>
+                <a href="#">Церковь</a>
+              </li>
+              <li>
+                <a href="#">Деньги</a>
+              </li>
+              <li>
+                <a href="#">Шоу-Бизнес</a>
+              </li>
+              <li>
+                <a href="#">Спорт</a>
+              </li>
+            </ul>
+          </nav>
+          <div class="lower-header">
+            <div class="lower-header-content">
+              <ul>
+                <li>
+                  <a href="#">Lorem ipsum dolor sit amet, consectetur</a>
+                </li>
+                <li>
+                  <a href="#">Lorem ipsum dolor sit amet, consectetur</a>
+                </li>
+              </ul>
+            </div>
+            <div class="lower-header-content">
+              <ul>
+                <li>
+                  <a href="#">Lorem ipsum dolor sit amet, consectetur</a>
+                </li>
+                <li>
+                  <a href="#">Lorem ipsum dolor sit amet, consectetur</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </header>
+      </div>
+      <div class="outer-div"></div>
+    </div>
+  </body>
+</html>
