5d3dbcc93c23b90632be3d397538fd30461ba627
unknown <ilyavusnya@gmail.com>
2020-02-02 13:31:43 +0200

    Commit

--- public/js/app.js
+++ public/js/app.js
@@ -19341,26 +19341,26 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /***/ }),
 
-/***/ "./resources/sass/main.scss":
-/*!**********************************!*\
-  !*** ./resources/sass/main.scss ***!
-  \**********************************/
+/***/ "./resources/sass/app.scss":
+/*!*********************************!*\
+  !*** ./resources/sass/app.scss ***!
+  \*********************************/
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-// removed by extract-text-webpack-plugin
+throw new Error("Module build failed (from ./node_modules/css-loader/index.js):\nModuleBuildError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\nError: ENOENT: no such file or directory, open 'D:\\php_projects\\strana\\resources\\sass\\app.scss'\n    at runLoaders (D:\\php_projects\\strana\\node_modules\\webpack\\lib\\NormalModule.js:316:20)\n    at D:\\php_projects\\strana\\node_modules\\loader-runner\\lib\\LoaderRunner.js:367:11\n    at D:\\php_projects\\strana\\node_modules\\loader-runner\\lib\\LoaderRunner.js:203:19\n    at process.nextTick (D:\\php_projects\\strana\\node_modules\\enhanced-resolve\\lib\\CachedInputFileSystem.js:85:15)\n    at process._tickCallback (internal/process/next_tick.js:61:11)");
 
 /***/ }),
 
 /***/ 0:
-/*!**************************************************************!*\
-  !*** multi ./resources/js/app.js ./resources/sass/main.scss ***!
-  \**************************************************************/
+/*!*************************************************************!*\
+  !*** multi ./resources/js/app.js ./resources/sass/app.scss ***!
+  \*************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
 __webpack_require__(/*! D:\php_projects\strana\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\php_projects\strana\resources\sass\main.scss */"./resources/sass/main.scss");
+module.exports = __webpack_require__(/*! D:\php_projects\strana\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
